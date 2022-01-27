3dd1082bd2ae393f9416284f48e72f0d2cd38345
levit123 <LeviCaseBlodgett@gmail.com>
2022-01-27 14:36:13 -0800

    HTML, CSS, and partial functions
    -added HTML elements to "index.html" and "app.component.html" for displaying the scores and the options of Rock, Paper, and Scissors
    -added CSS styling to "styles.css" and "app.component.css"
    -started adding functionality and fleshing out the class used in the application to the "app.component.ts"
    
    Next step is to finish the "clearField" function (and the other functions) in the "app.component.ts" file

--- rockpaperscissors/src/app/app.component.ts
+++ rockpaperscissors/src/app/app.component.ts
@@ -5,6 +5,41 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+//defines the properties of the class used in the projects
 export class AppComponent {
+  //variable for the human player's score
+  userScore = 0;
+  //variable for the computer player's score
+  compScore = 0;
+  //string variable for the human player's chosen option
+  userSelected: string;
+  //string varaible for the computer player's chosen option
+  compSelected: string;
+  action: string;
+  status: string;
+  //array containing the options the players can choose from
+  compWeapons = [
+  'rock',
+  'paper',
+  'scissors'
+  ];
+
+  //function for handling the user's chosen option
+  userPick(userWeapon: string): void {
+    this.userSelected = userWeapon;
+    console.log(this.userSelected);
+
+    setTimeout(() => {
+      const randomNum = Math.floor(Math.random() * 3);
+      this.compSelected = this.compWeapons[randomNum];
+      console.log(this.compSelected);
+      this.checkResult();
+    }, 1000)
+  }
+
+  clearField() {
+
+  }
+
   title = 'rockpaperscissors';
 }
