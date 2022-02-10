46c9d3d8eed7567afc61cbcbf0d49cc113df1db5
levit123 <LeviCaseBlodgett@gmail.com>
2022-02-10 14:24:04 -0800

    finished functions, tweaked settings, and fixed CSS
    -finished the functions in "app.component.ts"
    -adjusted settings in "tsconfig.json" to allow for implicit "any" type when defining variables/parameters/etc. in typescript
    -tweaked the "#user-label" CSS styling, added the "#comp-label" CSS styling
    
    Note for future reference: it seems when creating an Angular project and using typescript in it, Angular by default doesn't allow typescript to implicit variable types, which defeats the whole purpose of typescript.
    
    To fix the errors related to implicitly defined typescript variables, go into "tsconfig.json" and comment out this out:
    
    "strict": true,
    
    Then, add this line:
    
    "noImplicitAny": false,

--- rockpaperscissors/src/app/app.component.ts
+++ rockpaperscissors/src/app/app.component.ts
@@ -34,12 +34,60 @@ export class AppComponent {
       this.compSelected = this.compWeapons[randomNum];
       console.log(this.compSelected);
       this.checkResult();
-    }, 1000)
+    }, 1000);
   }
 
   clearField() {
-
+    setTimeout(() => {
+      this.status = '';
+      this.userSelected = '';
+      this.compSelected = '';
+    }, 1500);
   }
 
-  title = 'rockpaperscissors';
+  win(user, comp) {
+    this.userScore++;
+    this.userSelected = user;
+    this.compSelected = comp;
+    this.action = 'beats';
+    this.status = '. You win!';
+    this.clearField();
+  }
+
+  lose(user, comp) {
+    this.compScore++;
+    this.userSelected = user;
+    this.compSelected = comp;
+    this.action = 'loses to';
+    this.status = '. You lose!';
+    this.clearField();
+  }
+  
+  draw(user, comp) {
+    this.userSelected = user;
+    this.compSelected = comp;
+    this.action = 'and';
+    this.status = '. You draw!';
+    this.clearField();
+  }
+
+  checkResult() {
+    const userChoice = this.userSelected;
+    const compChoice = this.compSelected;
+    switch (userChoice + compChoice) {
+      case 'rockscissors':
+      case 'paperrock':
+      case 'scissorspaper':
+        this.win(userChoice, compChoice);
+        break;
+      case 'rockpaper':
+      case 'scissorsrock':
+      case 'paperscissors':
+        this.lose(userChoice, compChoice); 
+        break;
+      default:
+        this.draw(userChoice, compChoice);
+        break;
+    }
+  }
 }
